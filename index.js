6bf2c6b11d78f2b9d0376e488b25a7b191f65738
Aleksandr Kanunnikov <lifeart92@gmail.com>
2019-06-30 16:08:30 +0300

    hello

--- index.js
+++ index.js
@@ -1,5 +1,18 @@
 'use strict';
 
+const babelPlugin = require('./lib/babel-plugin');
+const { hasPlugin, addPlugin } = require('ember-cli-babel-plugin-helpers');
+const { cacheKeyForStableTree } = require('calculate-cache-key-for-tree');
 module.exports = {
-  name: require('./package').name
+  name: require('./package').name,
+  cacheKeyForTree: cacheKeyForStableTree,
+  included(parent) {
+    this._super.included.apply(this, arguments);
+    if (parent.env === 'production') {
+      let plugin = Object.assign(babelPlugin, { baseDir() { return __dirname; } })
+      if (!hasPlugin(parent, 'experimental-bundle-minifier')) {
+        addPlugin(parent, plugin);
+      }
+    }
+  }
 };
